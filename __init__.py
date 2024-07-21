17d2223e701742532b9c68a346753c6affb0956b
Ahmed ZOUITANE <zouitane.ahmed02@gmail.com>
2024-07-21 04:03:01 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,21 @@
+from .Man_os1 import touch, cat, nano, rm, mkdir, rmdir, ren, mv, cd, tree, pwd, ls, ls_l, istime, size, get_info, get_path
+
+__all__ = [
+    'touch',
+    'cat',
+    'nano',
+    'rm',
+    'mkdir',
+    'rmdir',
+    'ren',
+    'mv',
+    'cd',
+    'tree',
+    'pwd',
+    'ls',
+    'ls_l',
+    'istime',
+    'size',
+    'get_info',
+    'get_path'
+]
