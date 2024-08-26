d3ce36eab5001adeedb421e0d554aaad3b8e74b2
Ahmed ZOUITANE <zouitane.ahmed02@gmail.com>
2024-08-26 01:12:13 +0100

    Add files via upload

---
+++ FileMa/__init__.py
@@ -0,0 +1,52 @@
+# Import functions from FileMa
+from .FileMa import (
+    touch,
+    rm,
+    nano,
+    cat,
+    ren,
+    mv,
+    copy,
+    copyf,
+    mkdir,
+    rmdir,
+    mkdirs,
+    rmdirs,
+    copyd,
+    cd,
+    tree,
+    pwd,
+    ls,
+    ls_l,
+    istime,
+    size,
+    get_info,
+    get_path
+    
+)
+
+__all__ = [
+    "touch",
+    "rm",
+    "nano",
+    "cat",
+    "ren",
+    "mv",
+    "copy",
+    "copyf",
+    "mkdir",
+    "rmdir",
+    "mkdirs",
+    "rmdirs",
+    "copyd",
+    "cd",
+    "tree",
+    "pwd",
+    "ls",
+    "ls_l",
+    "istime",
+    "size",
+    "get_info",
+    "get_path"
+    
+]
